7300e05b5dcf600d39a33ccd2ab4642d186d3956
chirix33 <chirixteam@gmail.com>
2025-01-29 11:13:28 -0600

    initialize Next.js project with Tailwind CSS and ESLint configuration

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-red-500">
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default Home;
