977a6de5fabf67ec751bd85f14582e258bb80a11
chirix33 <chirixteam@gmail.com>
2025-02-09 15:15:17 -0600

    `Refactor: Remove redundant Footer imports and update styles`

---
+++ components/events/featured-event.tsx
@@ -0,0 +1,12 @@
+// import Link from "next/link";
+
+export const FeaturedEventSection = () => {
+    return (
+        <section className="w-full my-8 px-8 lg:px-24 max-w-6xl mx-auto">
+            <div className="my-4 w-full flex items-center justify-center gap-4">
+                <h3 className="flex-0 text-xl">Featured</h3>
+                <div className="flex-1 w-full h-[2px] bg-accent-color-primary rounded-lg"></div>
+            </div>
+        </section>
+    );
+}
