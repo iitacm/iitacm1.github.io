6d063ea383f30c8eea5b25cd219df3a579e3d307
chirix33 <chirixteam@gmail.com>
2025-02-18 15:25:09 -0600

    `Added LinkedIn SVG icon and updated team page layout`

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,4 +1,7 @@
 import { Header } from "@/components/ui/page-header";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { LinkedInSVG } from "../svgs";
 
 const Page = () => {
     const teamHeaderText = `Our dedicated team is committed to fostering innovation, collaboration, and a love for technology within the IIT community. From organizing impactful events to supporting members\’ growth and building a vibrant network of tech enthusiasts.`;
@@ -14,12 +17,82 @@ const Page = () => {
         <div className="my-16 px-8 lg:px-24">
             {/* Team Type (eg, Leadership Team) */}
             <div className="team">
-                <h3 className="mb-4">Leadership Team</h3>
-                <div className="board-members grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-12">
-                    <div className="bg-gray h-24 w-full"></div>
-                    <div className="bg-gray h-24 w-full"></div>
-                    <div className="bg-gray h-24 w-full"></div>
-                    <div className="bg-gray h-24 w-full"></div>
+                <h3 className="mb-8">Leadership Team</h3>
+                <div className="board-members grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-12">
+                    <div className="mx-auto md:mx-0 h-80 w-3/4">
+                        <div className="h-3/4 w-full">
+                            <Image src="/assets/acm_pictures/team/jason_new.jpg" width={300} height={400} className="w-full h-full object-cover" alt="Team Member" />
+                        </div>
+                        <div className="flex flex-col mt-2">
+                            <h3 className="text-light-red text-lg">President</h3>
+                            <p className="text-sm text-gray">Jason Zheng</p>
+                            <div className="mt-px text-sm flex justify-between w-full items-center">
+                                <div className="socials flex items-center gap-2">
+                                    <span className="text-fontcolor"><LinkedInSVG className="w-6 h-6" /></span>
+                                </div>
+                                <div>
+                                    <Button variant="default" className="text-sm bg-gray text-background hover:bg-fontcolor">More Info</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="h-80 w-3/4">
+                        <div className="h-3/4 w-full">
+                            <Image src="/assets/acm_pictures/team/jason_new.jpg" width={300} height={400} className="w-full h-full object-cover" alt="Team Member" />
+                        </div>
+                        <div className="flex flex-col mt-2">
+                            <h3 className="text-light-red text-lg">President</h3>
+                            <p className="text-sm text-gray">Jason Zheng</p>
+                            <div className="text-sm flex justify-between w-full items-center">
+                                <div className="socials flex items-center gap-2">
+                                    <span>X</span>
+                                    <span>LinkedIn</span>
+                                    <span>GitHub</span>
+                                </div>
+                                <div>
+                                    <Button variant="default" className="text-sm bg-gray text-background hover:bg-fontcolor">More Info</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-80 w-3/4">
+                        <div className="h-3/4 w-full">
+                            <Image src="/assets/acm_pictures/team/jason_new.jpg" width={300} height={400} className="w-full h-full object-cover" alt="Team Member" />
+                        </div>
+                        <div className="flex flex-col mt-2">
+                            <h3 className="text-light-red text-lg">President</h3>
+                            <p className="text-sm text-gray">Jason Zheng</p>
+                            <div className="text-sm flex justify-between w-full items-center">
+                                <div className="socials flex items-center gap-2">
+                                    <span>X</span>
+                                    <span>LinkedIn</span>
+                                    <span>GitHub</span>
+                                </div>
+                                <div>
+                                    <Button variant="default" className="text-sm bg-gray text-background hover:bg-fontcolor">More Info</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-80 w-3/4">
+                        <div className="h-3/4 w-full">
+                            <Image src="/assets/acm_pictures/team/jason_new.jpg" width={300} height={400} className="w-full h-full object-cover" alt="Team Member" />
+                        </div>
+                        <div className="flex flex-col mt-2">
+                            <h3 className="text-light-red text-lg">President</h3>
+                            <p className="text-sm text-gray">Jason Zheng</p>
+                            <div className="text-sm flex justify-between w-full items-center">
+                                <div className="socials flex items-center gap-2">
+                                    <span>X</span>
+                                    <span>LinkedIn</span>
+                                    <span>GitHub</span>
+                                </div>
+                                <div>
+                                    <Button variant="default" className="text-sm bg-gray text-background hover:bg-fontcolor">More Info</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </div>
