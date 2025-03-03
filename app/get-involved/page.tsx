0537ca9c4ecf7ba1a397bccc3cbb6235aa9e02a4
chirix33 <chirixteam@gmail.com>
2025-03-03 15:12:30 -0600

    `Added new section to get involved page with image and instructions on how to join ACM IIT`

--- app/get-involved/page.tsx
+++ app/get-involved/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/ui/page-header";
+import Image from "next/image";
 
 const Page = () => {
     const teamHeaderText = `Join ACM IIT to unlock opportunities for learning, networking, and hands-on experiences.`;
@@ -28,6 +29,17 @@ const Page = () => {
                 </ul>
             </section>
         </div>
+        <section className="relative mb-16 w-full h-[400px]">
+            <div className="px-8 lg:px-24 flex justify-center items-center">
+                <div className="z-[9] flex flex-col gap-8 font-raleway bg-fontcolor text-background rounded-lg p-8 w-full md:w-1/2">
+                    <h2 className="text-2xl text-center">How to Join ACM IIT</h2>
+                    <p className="text-sm font-lighter">
+                    Share a few details about yourself, and we’ll guide you through the next steps to officially become part of our chapter. Whether you&apos;re looking to learn, network, or lead, there&apos;s a place for you here. Let’s build something great together!
+                    </p>
+                </div>
+            </div>
+            <Image src="/assets/ribbon.png" width={1200} height={378} alt="Ribbon" className="absolute bottom-[150px] w-full h-[400px] object-cover" />
+        </section>
         </>
     );
 };
