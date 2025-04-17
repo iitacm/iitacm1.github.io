1aa79fade14bb3c8e981c475a5ef810aef56bdc4
Ashraf Abdul-Muumin <chirixteam@gmail.com>
2025-04-17 18:01:18 -0500

    refactor: update board members API to use database and modify package configuration

--- app/api/board-members/route.ts
+++ app/api/board-members/route.ts
@@ -1,19 +1,33 @@
-import { promises as fs } from "fs"
-import path from "path"
 import { NextResponse } from "next/server"
-import type { BoardMember } from "@/lib/interfaces"
+import { neon } from "@neondatabase/serverless";
 
 export async function GET() {
   try {
-    // Read the JSON file using the async version of fs
-    const filePath = path.join(process.cwd(), "data", "board_members.json")
-    const jsonData = await fs.readFile(filePath, "utf-8")
-
-    // Parse the JSON data
-    const boardMembers: BoardMember[] = JSON.parse(jsonData)
+    const sql = neon(process.env.ACM_IIT_POSTGRES_DATABASE_URL!);
+    const data = await sql`SELECT
+      b.id,
+      b.name,
+      b.position,
+      b.more_info,
+      b.picture,
+      b.year,
+      b.major,
+      b.minor,
+      b.category,
+      COALESCE(
+        (
+          SELECT jsonb_object_agg(ms.type, ms.url)
+          FROM member_socials ms
+          WHERE ms.member_id = b.id
+        ),
+        '{}'::jsonb
+      ) AS socials
+    FROM board_members b
+    ORDER BY b.name;`;
 
     // Return the data as JSON
-    return NextResponse.json(boardMembers)
+    return NextResponse.json(data);
+
   } catch (error) {
     console.error("Error fetching board members:", error)
     return NextResponse.json({ error: "Failed to fetch board members" }, { status: 500 })
